56239f934994b3913092c28a848129443cee2a40
btbsakib <btbsakibm@barytech.com>
2023-11-21 00:04:44 +0600

    location model

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,Location
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Location)
+
--- users/models.py
+++ users/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from localflavor.us.models import USStateField,USZipCodeField
 
 # Create your models here.
+class Location(models.Model):
+    address1=models.CharField(max_length=150,blank=True)
+    address2=models.CharField(max_length=150,blank=True)
+    city=models.CharField(max_length=50)
+    state=USStateField(default="NY")
+    zip_code=USZipCodeField(blank=True)
+    def __str__(self):
+        return self.address1
+
 class Profile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     photo=models.ImageField(null=True)
     bio=models.CharField(max_length=140,blank=True)
     phone_number=models.CharField(max_length=12,blank=True)
+    location=models.OneToOneField(Location,on_delete=models.CASCADE,null=True)
     def __str__(self):
         return self.user.username
